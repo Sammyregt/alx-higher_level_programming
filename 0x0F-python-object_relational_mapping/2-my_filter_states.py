324b7c6b9b203dc42920d29a14593c64d069c7e5
Sammyregt <adeoyesamuel100@gmail.com>
2022-12-27 15:35:49 +0000

    using pycodestyle format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                        db=sys.argv[3], port=3306)
+                         db=sys.argv[3], port=3306, host="localhost")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
                 id ASC".format(sys.argv[4]))
