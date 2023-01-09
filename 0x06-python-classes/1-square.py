eb266fee11e96d1fbcfea1f6f09a67f38d21bcef
Sammyregt <adeoyesamuel100@gmail.com>
2023-01-09 01:14:40 +0000

    test files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Define a class named square """
+
+
+class Square:
+    """ Representing the class """
+
+    def __init__(self, size):
+        """
+            A constructor of the square class
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
