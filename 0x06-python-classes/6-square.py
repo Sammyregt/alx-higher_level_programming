165825b33ec7c6eb45d701f844dadbc8a5ed0a07
Sammyregt <adeoyesamuel100@gmail.com>
2023-01-09 13:55:58 +0000

    update file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,8 +58,8 @@ class Square:
         if self.__size == 0:
             print("")
 
-        [print("") for i in range(0, self.__position[1])]
+        [print("$") for i in range(0, self.position[1])]
         for i in range(0, self.__size):
             [print("_", end="") for j in range(0, self.position[0])]
             [print("#", end="") for k in range(0, self.size)]
-            print("")
+            print("$")
