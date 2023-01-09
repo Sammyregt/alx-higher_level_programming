ebd695be5293259e22a9fdcdfb181c6c205f3b91
Sammyregt <adeoyesamuel100@gmail.com>
2023-01-09 13:59:00 +0000

    update file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,7 @@ class Square:
             print("")
 
         [print("") for i in range(0, self.position[1])]
-        for i in range(0, self.__size):
-            [print("_", end="") for j in range(0, self.position[0])]
+        for i in range(0, self.size):
+            [print("", end="") for j in range(0, self.position[0])]
             [print("#", end="") for k in range(0, self.size)]
             print("")
