1f5b11dbb459f8d2e44f9f06613e4575124ad02d
Sammyregt <adeoyesamuel100@gmail.com>
2024-01-23 23:32:05 +0100

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,11 +2,5 @@
 
 
 def no_c(my_string):
-    new_string = ""
-    
-    for char in my_string:
-        # Check if the character is not 'c' or 'C'
-        if char.lower() != 'c':
-            new_string += char
-
-    return new_string)
+    copy_str = [x for x in my_string if (x != 'c' or x != 'C')]
+    return ("".join(copy_str))
